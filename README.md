a50f2e3a2e39b4234994393f56665617a5484344
pl4nty <21111317+pl4nty@users.noreply.github.com>
2022-10-02 22:38:46 +1100

    feat: initial release

---
+++ README.md
@@ -0,0 +1,14 @@
+# Azure Favicons Browser Extension
+
+[![version](https://img.shields.io/github/manifest-json/v/pl4nty/azure-favicons?logo=github&logoColor=white)](https://github.com/pl4nty/azure-favicons/releases)
+<!-- [![Edge Add-on]()]() -->
+<!-- [![Chrome Extension](https://img.shields.io/chrome-web-store/v/foo?logo=google&logoColor=white)](https://chrome.google.com/webstore/detail/azure-favicons/foo) -->
+<!-- [![Firefox Add-on](https://img.shields.io/amo/v/azure-favicons?logo=mozilla&logoColor=white)](https://addons.mozilla.org/en-US/firefox/addon/azure-favicons) -->
+
+Tired of all your Azure or Intune browser tabs having the same favicon? This extension fixes that by setting the favicon to the appropriate one for the current service.
+
+Install links will be added when it's approved on the various browser marketplaces. In the meantime, you can [download the zip](https://github.com/pl4nty/azure-favicons/releases) and sideload it on [Edge](https://learn.microsoft.com/en-us/microsoft-edge/extensions-chromium/getting-started/extension-sideloading), [Chrome](https://developer.chrome.com/docs/extensions/mv2/getstarted/#manifest), and [Firefox](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension#installing).
+
+![Screenshot](https://raw.githubusercontent.com/pl4nty/azure-favicons/main/assets/screenshot2.png)
+
+Inspired by Maddison Hellstrom's excellent [AWS Favicons](https://github.com/b0o/aws-favicons-webextension/) extension.
---
+++ content.js
@@ -0,0 +1,62 @@
+let defaultIcon = document.querySelector("link[rel=icon]")?.href
+
+// CSS styling from _generated/Less/MsPortalImpl/Base/Base.Images.css
+let styles;
+fetch(chrome.runtime.getURL('msportalfx-svg.css')).then(res => res.text()).then(text => styles = text)
+
+function loadIcon() {
+  let refIcon = document.querySelector(".fxs-blade-header-icon use") // header
+  || document.querySelector(".fxc-gcflink-icon use") // table row - some don't have icons eg Subscriptions
+  || document.querySelector(".ext-overlay-image svg use") // optional overlay for table with no rows
+  || [...document.querySelectorAll('.fxs-portal-activated use')].pop() // blade without header eg Properties
+
+  if (refIcon) {
+    setSVGIcon(document.querySelector(refIcon.href.baseVal))
+  } else {
+    // MEM directly embeds SVGs
+    let embedIcon = document.querySelector(".fxs-blade-header-icon svg") || document.querySelector('.fxs-portal-activated svg')
+    if (embedIcon) {
+      setSVGIcon(embedIcon)
+    } else {
+      // some blades use external icons eg costmanagement
+      let externalIcon = document.querySelector(".fxs-blade-header-icon img")?.src
+      setIcon(externalIcon || defaultIcon)
+    }
+  } 
+}
+
+function setSVGIcon(svgRef) {
+  svgRef = svgRef.cloneNode(true)
+
+  // Required XML namespace
+  svgRef.setAttribute("xmlns", "http://www.w3.org/2000/svg")
+
+  // SVG definition dependencies eg linearGradients
+  svgRef.appendChild(document.querySelector('#DefsContainer defs').cloneNode(true))
+
+  let style = document.createElement('style')
+  style.innerHTML = styles
+  svgRef.appendChild(style)
+
+  // Hashes must be manually escaped: https://stackoverflow.com/a/63720894
+  setIcon("data:image/svg+xml,"+svgRef.outerHTML.replaceAll('symbol','svg').replaceAll('#','%23'))
+}
+
+function setIcon(icon) {
+  let link = document.querySelector("link[rel=icon]")
+  link.removeAttribute('type')
+  link.href = icon
+
+  let shortcut = document.querySelector("link[rel='shortcut icon']")
+  if (shortcut) {
+    shortcut.removeAttribute('type')
+    shortcut.href = icon
+  }
+}
+
+// Hashchange doesn't always fire when changing blades
+document.addEventListener('load', loadIcon, true);
+
+// Some blades don't fire onload eg bulk operation results
+// TODO is 500ms enough?
+document.addEventListener('click', () => setTimeout(loadIcon, 500), true);
